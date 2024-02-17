950065096511a0cf22b799b4959f2d5dc4c71084
onnitoff <onnitoff@gmail.com>
2024-02-17 20:37:11 +0300

    Добавьте файлы проекта.

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            string data = "some text";
+
+            return PartialView("DataPartial", data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
